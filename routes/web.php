bf0453d3b7599ff76acc3c752619d636594ef159
wikalsnri <wikalsnri@gmail.com>
2024-04-18 11:51:48 +0700

    kalkulator try out

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProsesController;
+
+Route::get('/', function () {
+    return view('kalkulator.index');
+});
+
+
+Route::resource('proses',ProsesController::class);
